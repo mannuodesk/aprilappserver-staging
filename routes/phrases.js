a74014ccf04845d1f443b386443fc267a3fe8ad8
sasr12 <rizvisyed12@gmail.com>
2017-01-11 19:53:31 +0500

    Deletion

--- routes/phrases.js
+++ routes/phrases.js
@@ -1,35 +1,49 @@
 var express = require('express');
 var router = express.Router();
-var mongoose= require('mongoose');
+var mongoose = require('mongoose');
 var Phrases = require('./../models/Phrases');
 var UrlUtility = require('./../Utility/UrlUtility');
 var Response = require('./../dto/APIResponse');
 var PhraseGroup = require('./../models/PhraseGroup');
 var Block = require('./../models/Block');
 
- //GET home page. 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//GET home page. 
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
 });
 
 var postPhrasesRoute = router.route('/addPhrase');
 var getAllPhrasesRoute = router.route('/getAllphrases');
+var deletePhrasesRoute = router.route('/deletePhrases/:_phraseId');
 var utility = new UrlUtility(
     {
     });
 // Connection URL. This is where your mongodb server is running.
-var url =utility.getURL();
+var url = utility.getURL();
 mongoose.connect(url, function (err, db) {
-    if(err)
-    {
+    if (err) {
         console.log("Failed to Connect to MongoDB");
     }
     else {
         console.log("Successfully Connected");
     }
 });
-
-postPhrasesRoute.post(function(req, res) {
+deletePhrasesRoute.get(function (req, res) {
+    var _phraseId = req.params._phraseId;
+    var response = new Response();
+    Phrases.findOne({ _id: _phraseId }
+        , function (err, phrase) {
+            if (err)
+                console.log(err);
+            else {
+                phrase.remove();
+                response.message = "Success";
+                response.code = 200;
+                res.json(response);
+            }
+        });
+});
+postPhrasesRoute.post(function (req, res) {
     // Create a new instance of the Beer model
     var phrases = new Phrases();
     var response = new Response();
@@ -41,10 +55,10 @@ postPhrasesRoute.post(function(req, res) {
     phrases._phraseGroupId = req.body._phraseGroupId;
     phrases.createdOnUTC = date;
     phrases.updatedOnUTC = date;
-    phrases.isDeleted = false; 
+    phrases.isDeleted = false;
     console.log(phrases);
     // Save the beer and check for errors
-    phrases.save(function(err) {
+    phrases.save(function (err) {
         if (err) {
             res.send(err);
         }
@@ -64,48 +78,40 @@ getAllPhrasesRoute.get(function (req, res) {
     var groupsArray = [];
     var array = [];
     var groupsBlockDto = {
-        'phraseGroup':PhraseGroup,
-        'phrases':[],
-        'block':Block
-    };       
+        'phraseGroup': PhraseGroup,
+        'phrases': [],
+        'block': Block
+    };
     PhraseGroup.find({}, null, { sort: { '_id': -1 } }, function (err, PhraseGroups) {
-        if (err)
-        {
+        if (err) {
             res.send(err);
         }
-        else
-        {
-            for(var i = 0; i< PhraseGroups.length; i++)
-            {
+        else {
+            for (var i = 0; i < PhraseGroups.length; i++) {
                 groupsBlockDto = {
-                    'phraseGroup':PhraseGroup,
-                    'phrases':[],
-                    'block':Block
-                };     
+                    'phraseGroup': PhraseGroup,
+                    'phrases': [],
+                    'block': Block
+                };
                 groupsBlockDto.phraseGroup = PhraseGroups[i];
                 var phraseGroupsId = PhraseGroups[i]._id;
                 console.log(phraseGroupsId);
                 var counter = 0;
                 array.push(groupsBlockDto);
-                Phrases.find({_phraseGroupId: phraseGroupsId}, function (err, phrases) {
-                    if (err)
-                    {
+                Phrases.find({ _phraseGroupId: phraseGroupsId }, function (err, phrases) {
+                    if (err) {
                         res.send(err);
                     }
-                    else
-                    {
+                    else {
                         console.log(phrases);
-                        if(phrases.length != 0)
-                        {
+                        if (phrases.length != 0) {
                             array[counter].phrases = phrases;
                         }
-                        else
-                        {
+                        else {
                             array[counter].phrases = [];
                         }
                         counter = counter + 1;
-                        if(counter == i)
-                        {
+                        if (counter == i) {
                             response.message = "Success";
                             response.code = 200;
                             response.data = array;
@@ -113,10 +119,9 @@ getAllPhrasesRoute.get(function (req, res) {
                         }
                     }
                 });
-                
+
             }
-            if(PhraseGroups.length == 0)
-            {
+            if (PhraseGroups.length == 0) {
                 response.message = "Failure";
                 response.code = 400;
                 res.json(response);
